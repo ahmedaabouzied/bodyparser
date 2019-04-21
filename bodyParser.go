7071f93a6fa72f8c1622cd4879ce0b734f1338fe
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-04-21 17:31:35 +0200

    Add proper comments
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- bodyParser.go
+++ bodyParser.go
@@ -1,3 +1,4 @@
+// Parses json reqeust body
 package bodyParser
 
 import (
@@ -6,12 +7,19 @@ import (
 	"net/http"
 )
 
+// Parse Jsob Request Body
+//
+// Returns a Map of String keys and Interface values
+//
 func JsonParse(r http.Response) (map[string]interface{}, error) {
+	// Read the r.body into a byte array
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return nil, err
 	}
+	// Make a map of String keys and Interface Values
 	b := make(map[string]interface{})
+	// Unmarshal the body array into the map
 	err = json.Unmarshal(body, &b)
 	if err != nil {
 		return nil, err
